2f56686944313b2d0007346cabd76aaa10131aa0
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-10-03 20:03:02 +0700

    tugas 1

---
+++ assignment-1.js
@@ -0,0 +1,14 @@
+let a = 100
+const b = 2
+
+let tambah = (a,b) => a+b 
+let kurang = (a,b) => a-b 
+let perkalian = (a,b) => a*b 
+const pembagian = (a,b) => a/b 
+let modulus = (a,b) => a%b
+
+console.log(`Pertambahan 100 + 2 = ${a+b}`) 
+console.log(`Pengurangan 100 - 2 = ${a-b}`) 
+console.log(`Perkalian 100 * 2 = ${a*b}`) 
+console.log(`Pembagian 100 / 2 = ${a/b}`) 
+console.log(`modulus 100 % 2 = ${a%b}`) 
